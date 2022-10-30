77380b24009fb527f9e8bf592bbf3ebd18eba3da
Linkjavier <linkjavier@hotmail.com>
2022-10-29 19:04:25 -0500

    Added 5 assertion

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,6 +1,5 @@
 void main(List<String> args) {
-  String str = "You Passed";
   assert(int.parse(args[0]) >= 80,
       'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
-  print(str);
+  print("You Passed");
 }
